41ff6ce13e8720e080744295a1670ead81aa2f7b
Damacy64 <80659223+Damacy64@users.noreply.github.com>
2025-02-25 11:18:42 -0600

    terminando UI

--- logica/extraerHeaders.py
+++ logica/extraerHeaders.py
@@ -1,11 +1,13 @@
 import re
 from tkinter import *
+from email.policy import default as DefaultPolicy
 from tkinter import filedialog
+from email.parser import Parser
 
 def extraer_encabezados(texto):
-    patron = r"(^[A-Za-z0-0-]+:.*?)(?=\n[A-Za-z0-9-]+:|\Z)"
-    encabezados_info = re.findall(patron, texto, re.MULTILINE | re.DOTALL)
-    return encabezados_info
+    msg = Parser(policy=DefaultPolicy).parsestr(texto)
+    cabecera = {key: value for key, value in msg.items()}
+    return cabecera
 
 def obtener_ruta():
     ruta_archivo = filedialog.askopenfilename(filetypes=[("Archivos de texto", "*.txt"), ("Todos los archivos", "*.*")])
@@ -23,22 +25,60 @@ def seleccionar_archivo(widgets_text):
             
             # Extraer encabezados con su información completa
             encabezados_info = extraer_encabezados(texto)
-        
+
+            security_results = {
+            "ARC": any(key in encabezados_info for key in ["ARC-Seal", "ARC-Message-Signature", "ARC-Authentication-Results"]),
+            "SPF": "Received-SPF" in encabezados_info,
+            "DKIM": "DKIM-Signature" in encabezados_info,
+            "DMARC": "Authentication-Results" in encabezados_info and "dmarc=pass" in encabezados_info["Authentication-Results"].lower()
+            }
+
             # Limpiar el widgets_text
             widgets_text.config(state="normal")
-            widgets_text.delete("1.0", "end")
-            widgets_text.insert("insert", texto)
+            widgets_text.delete("1.0", END)
+            
+            buscar_links(texto,widgets_text)
+            buscar_ip(texto,widgets_text)
+
+            widgets_text.insert("insert","\n Protocolos encontrados:\n")
+            for key, value in security_results.items():
+                widgets_text.insert("insert", f"{key}: {'✅ Presente' if value else '❌ No encontrado'}\n")
+                widgets_text.insert("insert", "-" * 85)
+
+            for clave, valor in encabezados_info.items():
+                widgets_text.insert("insert", f"{clave} : \n              {valor}\n")
+                widgets_text.insert("insert", "-" * 85)
             widgets_text.config(state="disabled")
 
             # Imprimir los encabezados con su contenido
-            for encabezado in encabezados_info:
-                widgets_text.insert(END, encabezado.strip() + "\n")  # Imprime cada encabezado con su información
         except FileNotFoundError:
             print(f"Error: El archivo '{ruta}' no existe.")
         except Exception as e:
             print(f"Error: {e}")
     else:
         print("error")
+
+def buscar_links(texto,widgets_text):
+    links = re.findall(r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', texto)
+    links = list(dict.fromkeys(links)) # Borramos los links que esten duplicados
+    links = list(filter(None, links))
+    if links:
+        widgets_text.insert("insert", " URLs encontradas:\n")
+        for link in links:
+            widgets_text.insert("insert", f" {link}\n")
+            widgets_text.insert("insert", ("-" * 50) + "\n")
+    else:
+        widgets_text.insert("insert","No hay URLs encontradas.\n")
+
+def buscar_ip(texto,widgets_text):
+    direcciones_ip = re.findall(r'\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b', texto)
+    if direcciones_ip:
+        widgets_text.insert("insert","\n IPs encontradas:\n")
+        for ip in direcciones_ip:
+            widgets_text.insert("insert",f" {ip}\n")
+            widgets_text.insert("insert",("-" * 50) + "\n") 
+    else:
+        widgets_text.insert("insert"," | No hay IPs encontradas.\n")
 
 def buscar_palabra(text_area,busqueda_entry):
     # Eliminar resaltados anteriores
